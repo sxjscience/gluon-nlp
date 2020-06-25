a98843aa23f2da8ecb80cc3f6e88e394c7a3d17d
Xingjian Shi <xshiab@connect.ust.hk>
2020-06-25 12:54:22 -0700

    Update test_auto_dataset.py

--- tests/test_auto_dataset.py
+++ tests/test_auto_dataset.py
@@ -62,18 +62,20 @@ def test_glue_datasets(task_name, feature_columns, label_columns):
         if task_name == 'mnli':
             dev_path = os.path.join(root, task_name, 'dev_mismatched.pd.pkl')
             test_path = os.path.join(root, task_name, 'test_mismatched.pd.pkl')
-            columns = ['sentence1', 'sentence2', 'label']
+            train_dev_columns = ['sentence1', 'sentence2', 'label']
+            test_columns = ['sentence1', 'sentence2']
         else:
             dev_path = os.path.join(root, task_name, 'dev.pd.pkl')
             test_path = os.path.join(root, task_name, 'test.pd.pkl')
-            columns = None
+            train_dev_columns = None
+            test_columns = None
         # We test for the parsing
-        train_data = TabularDataset(train_path, columns=columns)
+        train_data = TabularDataset(train_path, columns=train_dev_columns)
         dev_data = TabularDataset(dev_path,
-                                  columns=columns,
+                                  columns=train_dev_columns,
                                   column_properties=train_data.column_properties)
         test_data = TabularDataset(test_path,
-                                   columns=columns,
+                                   columns=test_columns,
                                    column_properties=train_data.column_properties)
         if expected_label_type is not None:
             assert train_data.column_properties[label_columns].type == expected_label_type
